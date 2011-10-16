7f1df2f75bc214c269d1fba1572d74cc09e42ed7
OHTA Shogo <athos0220@gmail.com>
2011-10-16 20:44:31 +0900

    added defn-iasm macro

--- src/iasm.clj
+++ src/iasm.clj
@@ -6,3 +6,6 @@
   (let [c (compile form)]
     `(let [f# (construct-proxy (Class/forName ~(.getName c)))]
        f#)))
+
+(defmacro defn-iasm [name args & body]
+  `(def ~name (fn-iasm ~args ~@body)))
